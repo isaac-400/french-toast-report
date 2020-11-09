e35ccc88c0b9f9ae133ef598fcf4f592a09fc014
Isaac <isaac.c.feldman.23@dartmouth.edu>
2020-11-09 12:08:17 -0500

    inital commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import requests, datetime, json, smtplib, ssl
+
+SMTP_SERVER = "localhost"
+PORT = 1025
+SENDER_EMAIL = "test@gmail.com"
+PASSWORD = "1234"
+USERS = {}
+
+
+def adduser(email, name):
+    USERS[email] = name
+    updateusers()
+
+def updateusers():
+    with open("users.json", 'w') as f:
+        json.dump(USERS, f)
+        f.close()
+
+def getusers():
+    global USERS
+    with open("users.json", 'r') as f:
+        USERS = json.load(f)
+        f.close()
+    return USERS
+
+def toasttoday():
+    menuID = 16
+    mealID = 1
+    serveDate = str(datetime.date.today())
+    url = "https://menuapi.dartmouth.edu:8314/api/HospitalitySuite/publishingGroups/GuestMenuData?menuId="+str(menuID)+"&mealId="+str(mealID)+"&serveDate="+serveDate
+    headers = { 'Accept': 'application/json', 'FacilityId':'53 Commons' }
+    params = { 'menuId': menuID, 'mealId': mealID, 'serveDate': serveDate}
+    response = requests.get(url, headers=headers, params=params)
+    json_response = response.json()[0]
+
+    for item in json_response["menuItems"]:
+        #print(item['recipe']["description"])
+        if item['recipe']["description"].__contains__("French Toast Sticks"):
+            return True
+
+
+
+if __name__ == '__main__':
+    if toasttoday():
+        server = smtplib.SMTP(SMTP_SERVER, PORT)
+        for addr, name in USERS.items():
+
+            message = """
+            Subject: French Toast Stick Report - {date}
+            
+            
+            Good morning {name}! French Toast Sticks will be served in foco today.
+            """
+            server.sendmail(SENDER_EMAIL, addr, message.format(date=datetime.date.today(), name=name))
+
+        server.quit()
+
